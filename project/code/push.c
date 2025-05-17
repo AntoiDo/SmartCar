59653e9ddcc7701371efb17d84fdfa4e3670ecc1
AntoiDo <cjhddb@foxmail.com>
2025-05-17 17:07:18 +0800

    添加了Readme

---
+++ project/code/push.c
@@ -0,0 +1,66 @@
+#include "push.h"
+
+uint8 Push_State = 0;   // 当检测到红色物块相关参数进入阈值范围时设置为1
+uint8 Stable_State = 0; // 当相关参数都在可控范围内就认为稳定了
+uint8 NOISE = 10;       // 判断状态的噪声范围
+
+volatile od_result_t od_result[10];
+
+/**
+ * @brief   检测红色方块
+ * @param   none
+ * @note    Push_State = 1 代表检测到红色方块
+ */
+void detectRedBlock()
+{
+    static uint8 count = 0;
+    uint8 pic_weight = od_result[0].res_x2 - od_result[0].res_x1; // 物块宽度
+    uint8 pic_height = od_result[0].res_y2 - od_result[0].res_y1; // 物块高度
+    // 判定条件1: y1 >= 43 && y2 == 100 超过十次, 且没有进入push状态
+    if (!Push_State && (od_result[0].res_y1 >= 45) && (od_result[0].res_y2 >= 88))
+    {
+        count++;
+        if (count > 10)
+        {
+            Push_State = 1;
+            count = 0;
+        }
+    }
+}
+
+/**
+ * @brief   获取红色方块在x轴上的中心偏移量
+ * @return  物块在x轴上的中心偏移量
+ * @note    结果为正值则物块偏右，负值则物块偏左
+ */
+int8 getCenterOffset_XAxis()
+{
+    uint8 center_offset = 0;
+    uint8 block_weight = od_result[0].res_x2 + od_result[0].res_x1; // 物块宽度
+    center_offset = block_weight / 2;                               // 物块中心
+    return center_offset - 50;
+}
+
+/**
+ * @brief   其实上获取的是物块的宽度，我想的是当接近物体时方框的宽度肯定变大
+ * @return  车子离物块的距离，大于0则需要继续前进这个数据
+ * @note    65是一个经验值，实际使用中可以根据需要进行调整
+ */
+int8 getCenterOffset_YAxis()
+{
+    uint8 center_offset = 0;
+    uint8 block_weight = od_result[0].res_x2 - od_result[0].res_x1; // 物块宽度
+    return 67 - block_weight;
+}
+
+void detectBlockStable()
+{
+    if (abs(getCenterOffset_XAxis()) < NOISE && abs(getCenterOffset_YAxis()) < NOISE)
+    {
+        Stable_State = 1;
+    }
+    else
+    {
+        Stable_State = 0;
+    }
+}
---
+++ project/code/push.h
@@ -0,0 +1,27 @@
+#ifndef PUSH_H
+#define PUSH_H
+
+#include "zf_common_headfile.h"
+
+#define PIC_WIDTH (160)
+#define PIC_HEIGHT (120)
+
+typedef struct
+{
+    uint8_t res_x1;
+    uint8_t res_y1;
+    uint8_t res_x2;
+    uint8_t res_y2;
+} od_result_t;
+
+extern volatile od_result_t od_result[10];
+
+extern uint8 Push_State;
+extern uint8 Stable_State;
+
+void detectRedBlock();
+void detectBlockStable();
+int8 getCenterOffset_XAxis();
+int8 getCenterOffset_YAxis();
+
+#endif
